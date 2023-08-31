b15bb08bfae027d516c9d56acc9faae341a497c8
Moiz-Ali-Khan <moizalikhan215@gmail.com>
2023-08-31 09:28:04 +0500

    Update

--- Exercises.py
+++ Exercises.py
@@ -1,11 +1,14 @@
+# Problem 1:
+# num_1, num_2 ,num_3 = input("Gave me 3 numbers: ").split(",")
+# average = (int(num_1)+int(num_2)+int(num_3))/3  
+# print(average)
 
-num_1, num_2 ,num_3 = input("Gave me 3 numbers: ").split(",")
-average = (int(num_1)+int(num_2)+int(num_3))/3  
-print(average)
+# Problem 2:
+# name = input("enter your name : ")
+# print(name[::-1])
 
-name = input("enter your name : ")
-print(name[::-1])
-
+# Problem 3:
 name, character = input("Enter name and character: ").split(",")
 print(f"length of the character is {len(name)}")
-print(f"Character Count is : {name.count(character)}")
+print(f"Character Count is : {name.lower().count(character.lower())}")
+
--- Notes.py
+++ Notes.py
@@ -1,6 +1,6 @@
 # Escape squence 
 # \",\n \t \\ \\\\
-# concatenation, str, *
+# concatenation, str(), *
 # input()---string, int, float
 # variable order
 # .split()---space, .split(",")---comma  
@@ -8,5 +8,5 @@
 # string index--[], negative index for last character and forward
 # string slicing--[start: stop(at -1 and less than this no): step(less than this no) and also has -1 for reverse]
 # len(), method---.lower(), .upper(), .title(), .count() 
-# 
+# For Removing Spaces-- .strip(), .lstrip(), .rstrip(), .replace(" "--entity,""--replacing entity)
 #
