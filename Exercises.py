88c197a14b86efbcd5c802dd993ea03dc73fe78c
Moiz-Ali-Khan <moizalikhan215@gmail.com>
2023-09-10 05:17:43 +0500

    changes

--- Exercises.py
+++ Exercises.py
@@ -1,14 +1,28 @@
+
 # Problem 1:
-num_1, num_2 ,num_3 = input("Gave me 3 numbers: ").split(",")
-average = (int(num_1)+int(num_2)+int(num_3))/3  
-print(average)
+# num_1, num_2 ,num_3 = input("Gave me 3 numbers: ").split(",")
+# average = (int(num_1)+int(num_2)+int(num_3))/3  
+# print(average)
 
 # Problem 2:
-name = input("enter your name : ")
-print(name[::-1])
+# name = input("enter your name : ")
+# print(name[::-1])
 
 # Problem 3:
-name, character = input("Enter name and character: ").split(",")
-print(f"length of the character is {len(name)}")
-print(f"Character Count is : {name.lower().count(character.lower())}")
+# name, character = input("Enter name and character: ").split(",")
+# print(f"length of the character is {len(name)}")
+# print(f"Character Count is : {name.lower().count(character.lower())}")
+
+# import random
+# Problem 4:
+# b = random.randint(0,9)
+# print(b)
+# a = int(input("give me a number:"))
+# if a == b:
+#     print("you won")
+# if a > b:
+#     print("too high")
+# else:
+#     print("too low")
+
 
--- Notes.py
+++ Notes.py
@@ -1,3 +1,7 @@
+# Git Commands: ls, clear, mkdir"spaces", cd, 
+# 
+# 
+
 # Escape squence 
 # \",\n \t \\ \\\\
 # concatenation, str(), *
@@ -8,5 +12,16 @@
 # string index--[], negative index for last character and forward
 # string slicing--[start: stop(at -1 and less than this no): step(less than this no) and also has -1 for reverse]
 # len(), method---.lower(), .upper(), .title(), .count() 
-# For Removing Spaces-- .strip(), .lstrip(), .rstrip(), .replace(" "--entity,""--replacing entity)
-#
+# For Removing Spaces-- .strip(), .lstrip(), .rstrip(), .replace(" "--entity,""--replacing entity, count of the replacing entity)
+# For finding position of a word or string -- .find(string or character, starting position)
+# For adding characters to string from left and right --center(len(string) + characters count, character)
+# Strings are immutable -- cannot change original string
+# Operators in string -- +, +=, *=
+# Control statements:
+# ------------------ if condition: 
+# ------------------ else: ---not possible with out if statement
+# ------------------ 
+# 
+# 
+#                   
+# Pass statement, == --for euality
