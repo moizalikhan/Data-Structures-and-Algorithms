edfcdd459fa2c318ba4fc294bb7a888a313d10f8
Moiz-Ali-Khan <moizalikhan215@gmail.com>
2023-09-13 06:53:52 +0500

    changes

--- Exercises.py
+++ Exercises.py
@@ -32,3 +32,106 @@
 # else:
 #     print("sorry, you cannot watch Coco")
 
+# Problem 6:
+# number = int(input("Enter a number for sum: :"))
+# i = 1
+# total = 0 
+# while i <=number: 
+#     total= total + i
+#     i = i+1
+# print(total)
+
+# problem 7:
+# number = input("Enter a Number: ")
+# total = 0
+# i = 0
+# while i <len(number):
+#     total = total + int(number[i])
+#     i = i + 1
+# print(total)
+
+# Problem 8:
+# name = input("Enter your name :")
+# i = 0
+# temp = ""
+# while i < len(name):
+#     if name[i] not in temp:
+#         print(f"{name[i]} : {name.count(name[i])}")
+#         temp = name[i]
+#     i = i+1
+
+# Problem 9:
+# Problem 4 extension:
+# import random
+# b = random.randint(0,10)
+# guess_game = False
+# i = 0
+# while not guess_game:
+#     a = int(input("give me a number:"))
+#     if a == b:
+#         i = i+1
+#         print(f"you won, you guessed in {i} times")
+#         continue
+#     if a > b:
+#         i = i+1
+#         print("too high")
+#         continue
+#     if a < b :
+#         i = i+1
+#         print("too low")
+#         continue
+
+# # problem 10:
+# def is_even(x):
+#     return x%2==0
+
+# problem 11:
+# def Greater(x,y):
+#     if x>y:
+#         return x 
+#     return y
+# a,b = input("enter: ").split(" ")
+# print(Greater(a,b))
+
+# problem 12:
+# def Greater(x,y,z):
+#     if x>y and x>z:
+#         return x
+#     if y>x and y>z:
+#         return x 
+#     return z
+# a,b,c = input("enter: ").split(" ")
+# print(Greater(a,b,c))
+
+# Problem 13:
+# def is_palindrome(string):
+#     new_string = ""
+#     count = len(string)
+#     while count>0:
+#             new_string = new_string + string[count - 1]
+#             count = count - 1
+#     return string == new_string
+# name = input("enter the name:")
+# print(is_palindrome(name))
+
+# Problem 14:
+# def Fibonacci_series(num):
+#     a,b = 0,1  
+#     i = 1
+#     print(a,end = " ")
+#     print(b,end = " ")
+#     while i <= int(num)-2:
+#         c = a+b
+#         print(c,end = " ")
+#         a = b
+#         b = c
+#         i += 1
+# x= input("Enter a number: ")
+# Fibonacci_series(x)
+
+# Problem 15:
+# 
+# 
+# 
+# 
+# 
--- Notes.py
+++ Notes.py
@@ -10,6 +10,7 @@
 # concatenation, str(), *
 # input()---string, int, float
 # variable order
+# print( ,end = " ") --on the same line with space
 
 
 # String Methods:
@@ -24,12 +25,31 @@
 # Strings are immutable -- cannot change original string
 # Operators in string -- +, +=, *=
 
+
 # Control statements:
 # ------------------ if condition:  or not condition --then check for empty or not 
 # ------------------ else: ---not possible with out if statement #also we can do nested if-else
 # ------------------ and operator, or operator 
 # ------------------ elif condition:
 # ------------------ while condition: increment -- while loop for which we don't know iterations and excute when the condition is true.
-# ------------------ 
-# Pass statement, in keyword --for checking 
+# ------------------ for i in range(num+1): --if we have a definite count of iterations --for(10,0,-1)--for reversing numbers
+# ------------------ range(staring num, ending num+1, Step Argument) --range function, --for i in string_name or variable_name:
+# Pass ,break--breaking the loop, continue--continuing the loop-- keywords
 # == --for euality
+# True, False -- Boolean in python 
+# Dry--Don't Repeat Yourself
+
+
+# Functions:
+# def name(parameters)-- function definiion
+# return -- use more than print
+# ="23" or None --as Default parameter
+# Local and Global Scope --for understanding
+
+# Lists:
+# ["word1", "word2", "1"]--ordered collection of items
+# [i]-- for accessing members
+# .append()-- for adding data last to your list
+# 
+# 
+# 
