85153cbd44ba7f4279446d976b242535f5817d0f
Moiz-Ali-Khan <moizalikhan215@gmail.com>
2023-10-01 20:55:52 +0500

    changes

--- Leetcode_questions.py
+++ Leetcode_questions.py
@@ -1,94 +1,104 @@
-# Contains Duplicate
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
-        return False
+# # Contains Duplicate
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         hashset = set()
+#         for n in nums:
+#             if n in hashset:
+#                 return True
+#             hashset.add(n)
+#         return False
 
-# sum of elemnts with target:
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        previousmap = {}
-        for i, n in enumerate(nums):
-            difference = target - n
-            if difference in previousmap:
-                return [previousmap[difference], i]
-            previousmap[n] = i
+# # sum of elemnts with target:
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         previousmap = {}
+#         for i, n in enumerate(nums):
+#             difference = target - n
+#             if difference in previousmap:
+#                 return [previousmap[difference], i]
+#             previousmap[n] = i
 
-# is palindrome by two pointers:
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        l , r = 0, len(s)-1
-        while l<r:
-            while l < r and not self.alpnum(s[l]):
-                l = l+1                
-            while r>l and not self.alpnum(s[r]):
-                r = r-1
-            if s[l].lower() != s[r].lower():
-                return False
-            l , r = l+1, r-1
-        return True
-    def alpnum(self, c):
-        return (ord("A") <= ord(c) <=ord("Z")or
-        (ord("a") <= ord(c) <=ord("z"))or 
-        (ord("0") <= ord(c) <=ord("9")))
+# # is palindrome by two pointers:
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         l , r = 0, len(s)-1
+#         while l<r:
+#             while l < r and not self.alpnum(s[l]):
+#                 l = l+1                
+#             while r>l and not self.alpnum(s[r]):
+#                 r = r-1
+#             if s[l].lower() != s[r].lower():
+#                 return False
+#             l , r = l+1, r-1
+#         return True
+#     def alpnum(self, c):
+#         return (ord("A") <= ord(c) <=ord("Z")or
+#         (ord("a") <= ord(c) <=ord("z"))or 
+#         (ord("0") <= ord(c) <=ord("9")))
 
-# when to buy and sell a stock
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0,1
-        Maxprofit = 0
-        while r < len(prices):
-            if prices[l]<prices[r]:
-                profit = prices[r]-prices[l]
-                Maxprofit = max(profit,Maxprofit)
-            else:
-                l = r
-            r += 1
-        return Maxprofit
+# # when to buy and sell a stock
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         l, r = 0,1
+#         Maxprofit = 0
+#         while r < len(prices):
+#             if prices[l]<prices[r]:
+#                 profit = prices[r]-prices[l]
+#                 Maxprofit = max(profit,Maxprofit)
+#             else:
+#                 l = r
+#             r += 1
+#         return Maxprofit
 
-# Array rotation
-def rotateLeft(d, arr):
-    # Write your code here
-    rotations = (d % len(arr))+1
-    reversed_arr =arr[rotations:] + arr[:rotations]
-    return reversed_arr
-arr = [1,2,3,4,5,6,7]
-d  = 3
-print(f"{rotateLeft(d,arr)} is the rotated one")
+# # Array rotation
+# def rotateLeft(d, arr):
+#     # Write your code here
+#     rotations = (d % len(arr))+1
+#     reversed_arr =arr[rotations:] + arr[:rotations]
+#     return reversed_arr
+# arr = [1,2,3,4,5,6,7]
+# d  = 3
+# print(f"{rotateLeft(d,arr)} is the rotated one")
 
-# swiping the elements of an array
-arr = [1,2,3,4,5]
-i = 0
-index1 = 0
-index2 = 2
-for i in arr:
-    temp = arr[index1]
-    arr[index1] = arr[index2]
-    arr[index2] = temp 
-print(arr)
+# # swiping the elements of an array
+# arr = [1,2,3,4,5]
+# i = 0
+# index1 = 0
+# index2 = 2
+# for i in arr:
+#     temp = arr[index1]
+#     arr[index1] = arr[index2]
+#     arr[index2] = temp 
+# print(arr)
 
-# max elements of an array
-arr = [1,2,3,4,5]
-i = 0
-max_element = 0
-while i <len(arr):
-    if max_element < arr[i]:
-        max_element = arr[i]
-    i +=1
-print(max_element)
+# # max elements of an array
+# arr = [1,2,3,4,5]
+# i = 0
+# max_element = 0
+# while i <len(arr):
+#     if max_element < arr[i]:
+#         max_element = arr[i]
+#     i +=1
+# print(max_element)
 
-# reverse of an array
-arr = [1,2,3,4,5]
-left_pointer = 0
-right_pointer = len(arr)-1
-while left_pointer <= right_pointer:
-    arr[left_pointer], arr[right_pointer] = arr[right_pointer],arr[left_pointer]
-    left_pointer +=1
-    right_pointer -=1
-print(arr)
+# # reverse of an array
+# arr = [1,2,3,4,5]
+# left_pointer = 0
+# right_pointer = len(arr)-1
+# while left_pointer <= right_pointer:
+#     arr[left_pointer], arr[right_pointer] = arr[right_pointer],arr[left_pointer]
+#     left_pointer +=1
+#     right_pointer -=1
+# print(arr)
 
+# # Contains not Duplicate in a sorted array
+# arr = [1,1,2,2,3,4,4,5,5]
+# l, r = 0,1
+# while l < len(arr) and r < len(arr):
+#     if arr[l] != arr[r]:
+#         one_time = arr[l]
+#         print(one_time)
+#         break
+#     l= l + 2
+#     r= r + 2
 
